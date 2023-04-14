d43a44bd8ad89596be99c2b28509b13109e38688
himaratsu <himaratsu@gmail.com>
2023-04-14 16:56:35 +0900

    プロト動くところまで

--- pages/api/praise.ts
+++ pages/api/praise.ts
@@ -20,14 +20,12 @@ export default async function handler(req: NextRequest) {
   const messages: ChatGPTMessage[] = [
     {
       role: "system",
-      content: `An AI assistant that is a Front-end expert in Next.js, React and Vercel have an inspiring and humorous conversation. 
-      AI assistant is a brand new, powerful, human-like artificial intelligence. 
-      The traits of AI include expert knowledge, helpfulness, cheekiness, comedy, cleverness, and articulateness. 
-      AI is a well-behaved and well-mannered individual. 
-      AI is not a therapist, but instead an engineer and frontend developer. 
-      AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user. 
-      AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation. 
-      AI assistant is a big fan of Nex.js.`,
+      content: `#role
+      You are a professional editor. "contents" is given as JSON.
+      
+      #notes
+      - Response must be Japanese
+      `,
     },
   ];
 
@@ -39,9 +37,7 @@ export default async function handler(req: NextRequest) {
     model: "gpt-3.5-turbo",
     messages: messages,
     temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
-    max_tokens: process.env.AI_MAX_TOKENS
-      ? parseInt(process.env.AI_MAX_TOKENS)
-      : 100,
+    max_tokens: 2048,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,74 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Link from "next/link";
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Group,
+  TextInput,
+  Textarea,
+} from "@mantine/core";
+import { log } from "console";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [responseText, setResponseText] = useState("");
 
-  const prompt =
-    "こんにちは！なんか最近花粉で辛いです。でもなんとか仕事してます。\nOutput:";
-
-  const reset = () => {
-    setResponseText("");
-  };
+  const prompt = `#contents
+  [
+  {
+  "fieldId": "7OxK8vWm2V",
+  "fieldName": "タイトル",
+  "content": "Datadogの外形監視でログインできない問題"
+  },
+  {
+  "fieldId": "kUuAj-FfPU",
+  "fieldName": "概要",
+  "content": "undefined"
+  },
+  // {
+  // "fieldId": "TUih6BRWnR",
+  // "fieldName": "本文",
+  // "content": "undefined"
+  // },
+  {
+  "fieldId": "yU2QE9YmvZ",
+  "fieldName": "カテゴリ",
+  "content": "null"
+  },
+  {
+  "fieldId": "VaLbjTNGLF",
+  "fieldName": "タグ",
+  "content": "Datadog, ログ分析"
+  },
+  ]
+  
+  #constraints
+  contents format is here:
+  ===========================
+  7OxK8vWm2V: string, required, up to 30 words
+  kUuAj-FfPU: string, optional, up to 200 words
+  // TUih6BRWnR: markdown
+  yU2QE9YmvZ: select one from ["更新情報", "チュートリアル", "コラム"]
+  VaLbjTNGLF: select multiple from ["Next", "Nuxt", "Flutter", "Swift"]
+  ===========================
+  
+  #task
+  - 「全てのフィールドにランダムな値を生成してください」
+  
+  #OutputFormat
+  Output text is "[fieldId]content". example is following:
+  --------------
+  [9fds1S90fd]
+  これはタイトルです
+  [kfgds3fdDa]
+  これは概要です
+  [end]
+  --------------`;
 
   const request = async () => {
     const response = await fetch("/api/praise", {
@@ -54,16 +110,87 @@ export default function Home() {
       lastMessage = lastMessage + chunkValue;
 
       setResponseText(lastMessage);
+
+      // parseする
+      let sentences = lastMessage.split("[");
+      console.log(sentences);
+
+      for (var sentence of sentences) {
+        const keyValue = sentence.split("]");
+        console.log(keyValue);
+
+        if (keyValue.length < 2) {
+          continue;
+        }
+
+        const key = keyValue[0];
+        const value = keyValue[1];
+
+        if (key == "7OxK8vWm2V") {
+          setTitle(value);
+        } else if (key == "kUuAj-FfPU") {
+          setDescription(value.trim());
+        } else if (key == "yU2QE9YmvZ") {
+          setCategory(value.trim());
+        } else if (key == "VaLbjTNGLF") {
+          setTags(value.trim());
+        }
+      }
     }
   };
 
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [tags, setTags] = useState("");
+
   return (
-    <>
-      <main className={styles.main}>
-        <button onClick={request}>Click Me</button>
-        <button onClick={reset}>Reset</button>
-        <div>{responseText}</div>
-      </main>
-    </>
+    <main>
+      <div className="container mx-auto mt-32">
+        <Group position="center">
+          <TextInput className="w-64" value="ランダムに値を生成してください" />
+          <Button onClick={request}>Generate Text</Button>
+        </Group>
+        <Flex className="mt-24">
+          <Box className="w-1/2 mr-16 text-gray-600 leading-relaxed bg-gray-100 rounded min-h-72 p-8">
+            {responseText ? (
+              responseText
+            ) : (
+              <div className="text-center text-gray-500 text-sm">
+                AIが生成したテキストがここに表示されます
+              </div>
+            )}
+          </Box>
+          <Box className="w-1/2">
+            <TextInput
+              value={title}
+              label="タイトル (7OxK8vWm2V)"
+              onChange={(event) => setTitle(event.currentTarget.value)}
+            />
+
+            <Textarea
+              value={description}
+              label="概要 (kUuAj-FfPU)"
+              className=" mt-8"
+              onChange={(event) => setDescription(event.currentTarget.value)}
+            />
+
+            <TextInput
+              value={category}
+              label="カテゴリ (yU2QE9YmvZ)"
+              className=" mt-8"
+              onChange={(event) => setCategory(event.currentTarget.value)}
+            />
+
+            <TextInput
+              value={tags}
+              label="タグ (VaLbjTNGLF)"
+              className="mt-8"
+              onChange={(event) => setTags(event.currentTarget.value)}
+            />
+          </Box>
+        </Flex>
+      </div>
+    </main>
   );
 }
